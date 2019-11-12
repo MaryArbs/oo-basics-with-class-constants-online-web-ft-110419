be571b1d40c94c6a16ca559f68506348547a4b98
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-12 15:27:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,19 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
 BRANDS=[]
+
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
+
+  def brand=)
+  end 
 
   def cobble
     self.condition = "new"
